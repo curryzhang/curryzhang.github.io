867bde996fea36f751b5eb9846929fc70e7098ba
curryzhang@rektec.com <curryzhang@rektec.com.cn>
2018-08-30 12:43:00 +0800

    测试

--- static/js/4.a13ec0352006bd00ad10.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{"3GDU":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("div",[t("a",[this._v("Page1")])])])}]},s=n("VU/8")({data:function(){return{}},methods:{}},r,!1,null,null,null);t.default=s.exports}});
-//# sourceMappingURL=4.a13ec0352006bd00ad10.js.map
--- static/js/5.1d30fd751adafb7c0956.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{"1uwG":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("div",[t("a",[this._v("Page2")])])])}]},s=n("VU/8")({data:function(){return{}},methods:{}},r,!1,null,null,null);t.default=s.exports}});
-//# sourceMappingURL=5.1d30fd751adafb7c0956.js.map
