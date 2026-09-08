ea7897beba7047cb5a5b4944f53b9f2e9dd3c734
钞票很多张 <zcd88773664@163.com>
2026-09-08 23:25:36 +0800

    update src/router.js

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Home from './views/Home.vue'
 import PostDetail from './views/PostDetail.vue'
 import Moments from './views/Moments.vue'
 import Admin from './views/Admin.vue'
+import CategoryView from './views/CategoryView.vue'
 
 // 使用 hash 路由：GitHub Pages 默认没有 SPA fallback，
 // 用 #/post/xxx 这种形式刷新/直链都不会 404。
@@ -10,6 +11,7 @@ const routes = [
   { path: '/', name: 'home', component: Home, meta: { title: '首页' } },
   { path: '/post/:slug', name: 'post', component: PostDetail, meta: { title: '文章' } },
   { path: '/moments', name: 'moments', component: Moments, meta: { title: '说说' } },
+  { path: '/category/:name', name: 'category', component: CategoryView, meta: { title: '代码笔记' } },
   { path: '/admin', name: 'admin', component: Admin, meta: { title: '管理' } }
 ]
 
